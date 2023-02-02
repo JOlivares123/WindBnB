f308fd7add74e0321608216a8f089bf59d45ae76
JOlivares123 <jacobolivares123@gmail.com>
2023-02-01 16:52:01 -0800

    implemented search button - needs functionality

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -1,9 +1,19 @@
+import {FaSearch} from 'react-icons/fa'
+
+import './Search.css'
+
 export const Search = () => {
     return(
-        <div className="btn-group">
-            <button type='button'> Location</button>
-            <button type='button'>Num of Guests</button>
-            <button type="button"> search icon</button>
+        <div className="btn-group shadow" role='group'>
+            <button type='button' className="btn reg-btn btn-outline-secondary">
+                <span className='text-black'>Location</span>
+            </button>
+            <button type='button' className="btn reg-btn btn-outline-secondary">
+                <span className='light'>Add Guests</span>
+            </button>
+            <button type="button" className="btn btn-outline-secondary srch-btn">
+                <FaSearch className='mb-1' color='#ef7b7b'/>
+            </button>
         </div>
     )
 }
